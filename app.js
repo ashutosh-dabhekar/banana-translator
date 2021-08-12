bd2c71e8f4490c110533f665fd45a7538bdd7729
Ashutosh Dabhekar <ashutosh.dabhekar99@gmail.com>
2021-08-12 20:15:13 +0530

    Translate partially engine done

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var btnTranslator=document.querySelector("#btn-translate")
 var txtInput=document.querySelector("#text-input")
+var outputDiv=document.querySelector("#output")
 
-console.log(txtInput);
 
 function clickHandler(){
-    console.log("clicked")
-    console.log("input",txtInput.value)
+    outputDiv.innerText="ajsjsjsjsjs "+ txtInput.value;
 }
 
 btnTranslator.addEventListener("click",clickHandler)
